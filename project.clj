d063f0ef4f79291668dd9bf9bff1994e16513807
Chris Nuernberger <chris@techascent.com>
2020-11-29 15:50:17 -0700

    Setting the license unambiguously.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject cnuernber/julia-clj "0.1.0-SNAPSHOT"
   :description "Julia bindings for Clojure."
   :url "https://github.com/cnuernber/libjulia-clj"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+  :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [cnuernber/dtype-next "6.00-beta-8"]
